3b68cd609d020590bbdcb07d4fbd2639c675c4bc
Mikhail Alyogin <kcasctiv@hotmail.com>
2019-01-26 23:09:06 +0400

    Add MultiPoint unmarshaling

--- multi_point.go
+++ multi_point.go
@@ -2,19 +2,25 @@ package ewkb
 
 import (
 	"database/sql/driver"
+	"errors"
 	"fmt"
 
 	"github.com/kcasctiv/go-ewkb/geo"
 )
 
+// MultiPoint presents MultiPoint geometry object
 type MultiPoint struct {
 	header
 	mp geo.MultiPoint
 }
 
+// Point returns point with specified index
 func (p *MultiPoint) Point(idx int) geo.Point { return p.mp.Point(idx) }
-func (p *MultiPoint) Len() int                { return p.mp.Len() }
 
+// Len returns length of MultiPoint (count of points)
+func (p *MultiPoint) Len() int { return p.mp.Len() }
+
+// String returns WKT/EWKT geometry representation
 func (p *MultiPoint) String() string {
 	var s string
 	if p.HasSRID() {
@@ -31,13 +37,28 @@ func (p *MultiPoint) String() string {
 	return s + " " + printMultiPoint(p, p.HasZ(), p.HasM())
 }
 
+// Scan implements sql.Scanner interface
 func (p *MultiPoint) Scan(src interface{}) error {
-	// TODO:
-	return nil
+	return scanGeometry(src, p)
 }
 
+// Value implements sql driver.Valuer interface
 func (p *MultiPoint) Value() (driver.Value, error) {
 	return p.String(), nil
+}
+
+// UnmarshalBinary implements encoding.BinaryUnmarshaler interface
+func (p *MultiPoint) UnmarshalBinary(data []byte) error {
+	h, byteOrder, offset := readHeader(data)
+	if h.Type() != MultiPointType {
+		return errors.New("not expected geometry type")
+	}
+
+	p.header = h
+
+	var err error
+	p.mp, _, err = readMultiPoint(data[offset:], byteOrder, getReadPointFunc(h.wkbType))
+	return err
 }
 
 func printMultiPoint(p geo.MultiPoint, hasZ, hasM bool) string {
