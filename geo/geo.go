7822b84068a9c67bbd3ef15f5a2c9524b9aa11f2
Mikhail Alyogin <kcasctiv@hotmail.com>
2019-01-30 20:44:40 +0400

    Add docs for geo package

--- geo/geo.go
+++ geo/geo.go
@@ -1,9 +1,17 @@
+// Package geo contains interfaces
+// of simple geometry objects,
+// and their creation methods
 package geo
 
+// Point presents interface of point
 type Point interface {
+	// X returns value of X dimension
 	X() float64
+	// Y returns value of X dimension
 	Y() float64
+	// Z returns value of X dimension
 	Z() float64
+	// M returns value of X dimension
 	M() float64
 }
 
@@ -14,6 +22,7 @@ func (p point) Y() float64 { return p.y }
 func (p point) Z() float64 { return 0 }
 func (p point) M() float64 { return 0 }
 
+// NewPoint returns new 2 dimensions point
 func NewPoint(x, y float64) Point {
 	return point{x, y}
 }
@@ -25,6 +34,7 @@ func (p pointZ) Y() float64 { return p.y }
 func (p pointZ) Z() float64 { return p.z }
 func (p pointZ) M() float64 { return 0 }
 
+// NewPointZ returns new 3 dimensions point with Z dimension
 func NewPointZ(x, y, z float64) Point {
 	return pointZ{x, y, z}
 }
@@ -36,6 +46,7 @@ func (p pointM) Y() float64 { return p.y }
 func (p pointM) Z() float64 { return 0 }
 func (p pointM) M() float64 { return p.m }
 
+// NewPointM returns new 3 dimensions point with M dimension
 func NewPointM(x, y, m float64) Point {
 	return pointM{x, y, m}
 }
@@ -47,12 +58,16 @@ func (p pointZM) Y() float64 { return p.y }
 func (p pointZM) Z() float64 { return p.z }
 func (p pointZM) M() float64 { return p.m }
 
+// NewPointZM returns new 4 dimensions point
 func NewPointZM(x, y, z, m float64) Point {
 	return pointZM{x, y, z, m}
 }
 
+// MultiPoint presents interface of multi point
 type MultiPoint interface {
+	// Point returns point with specified index
 	Point(int) Point
+	// Len returns count of points
 	Len() int
 }
 
@@ -63,12 +78,16 @@ type multiPoint struct {
 func (p multiPoint) Point(idx int) Point { return p.points[idx] }
 func (p multiPoint) Len() int            { return len(p.points) }
 
+// NewMultiPoint returns new multi point
 func NewMultiPoint(points []Point) MultiPoint {
 	return multiPoint{points}
 }
 
+// Polygon presents interface of polygon
 type Polygon interface {
+	// Ring returns ring with specified index
 	Ring(int) MultiPoint
+	// Len returns count of rings
 	Len() int
 }
 
@@ -79,12 +98,16 @@ type polygon struct {
 func (p polygon) Ring(idx int) MultiPoint { return p.rings[idx] }
 func (p polygon) Len() int                { return len(p.rings) }
 
+// NewPolygon returns new polygon
 func NewPolygon(rings []MultiPoint) Polygon {
 	return polygon{rings}
 }
 
+// MultiLine presents interface of multi line
 type MultiLine interface {
+	// Line returns line with specified index
 	Line(int) MultiPoint
+	// Len returns count of lines
 	Len() int
 }
 
@@ -95,12 +118,16 @@ type multiLine struct {
 func (l multiLine) Line(idx int) MultiPoint { return l.lines[idx] }
 func (l multiLine) Len() int                { return len(l.lines) }
 
+// NewMultiLine returns new multi line
 func NewMultiLine(lines []MultiPoint) MultiLine {
 	return multiLine{lines}
 }
 
+// MultiPolygon presents interface of multi polygon
 type MultiPolygon interface {
+	// Polygon returns polygon with specified index
 	Polygon(int) Polygon
+	// Len returns count of polygons
 	Len() int
 }
 
@@ -111,6 +138,7 @@ type multiPolygon struct {
 func (p multiPolygon) Polygon(idx int) Polygon { return p.pols[idx] }
 func (p multiPolygon) Len() int                { return len(p.pols) }
 
+// NewMultiPolygon returns new multi polygon
 func NewMultiPolygon(pols []Polygon) MultiPolygon {
 	return multiPolygon{pols}
 }
